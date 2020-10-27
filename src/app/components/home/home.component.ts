4c9f4f24a8ed01b8c87adf677e2ea86ab12633b1
amylokh <amylokh@gmail.com>
2020-10-27 15:36:14 +0530

    Fix intermittent disappearance of pie chart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,12 +13,27 @@ export class HomeComponent implements OnInit {
   totalActive = 0;
   totalRecovered = 0;
   globalData : GlobalDataSummary[];
-
+  loaded = false;
+  
   public doughnutChartLabels: string[] = [];
   public doughnutChartData: number[] = [];
   chartOptions = {
     responsive: true
   };
+
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
 
   constructor(private dataService: DataServiceService) { }
 
@@ -34,10 +49,11 @@ export class HomeComponent implements OnInit {
               this.totalDeaths += countryData.deaths;
               this.totalRecovered += countryData.recovered;
 
-              this.doughnutChartLabels.push(countryData.country);
               this.doughnutChartData.push(countryData.confirmed);
+              this.doughnutChartLabels.push(countryData.country);
             }
           })
+          this.loaded = true;
         }
       }
     )
