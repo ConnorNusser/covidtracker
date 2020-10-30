f0fb4e9804b8ac2ec370ad5ee6f8af6ef1999b63
amylokh <amylokh@gmail.com>
2020-10-30 09:36:32 +0530

    Format numbers properly

--- src/app/components/dashboard-card/dashboard-card.component.ts
+++ src/app/components/dashboard-card/dashboard-card.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-card',
   templateUrl: './dashboard-card.component.html',
   styleUrls: ['./dashboard-card.component.css']
 })
-export class DashboardCardComponent implements OnInit {
+export class DashboardCardComponent implements OnInit, OnChanges {
 
   @Input('totalConfirmed')
   totalConfirmed;
@@ -19,6 +19,39 @@ export class DashboardCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  ngOnChanges() {
+    this.totalActive = this.convertNumber(this.totalActive);
+    this.totalConfirmed = this.convertNumber(this.totalConfirmed);
+    this.totalDeaths = this.convertNumber(this.totalDeaths);
+    this.totalRecovered = this.convertNumber(this.totalRecovered);    
+  }
+
+  convertNumber(x) {
+    if (isNaN(x)) return x;
+
+    if (x < 9999) {
+      return x;
+    }
+
+    if (x < 1000000) {
+      return Math.round(x / 1000) + "K";
+    }
+    if (x < 10000000) {
+      return (x / 1000000).toFixed(2) + "M";
+    }
+
+    if (x < 1000000000) {
+      return (x / 1000000).toFixed(2) + "M";
+    }
+
+    if (x < 1000000000000) {
+      return Math.round((x / 1000000000)) + "B";
+    }
+
+    return "1T+";
   }
 
 }
