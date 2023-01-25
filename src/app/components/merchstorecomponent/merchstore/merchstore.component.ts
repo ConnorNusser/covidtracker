283b9291c665ddfcda7d19092a0fa563d80b22be
Connor <connornusser@gmail.com>
2023-01-24 18:14:57 -0600

    lmk

--- src/app/components/merchstorecomponent/merchstore/merchstore.component.ts
+++ src/app/components/merchstorecomponent/merchstore/merchstore.component.ts
@@ -6,12 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merchstore.component.css']
 })
 export class MerchstoreComponent implements OnInit {
-  
+  merchItems: string[] = [];
   constructor() { }
 
   ngOnInit() {
   }
-  onclick(){
+  onclick(box: string){
+    this.merchItems.push(box);
+    console.log(box);
     console.log("hello");
   }
 }
